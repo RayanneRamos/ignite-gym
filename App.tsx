d81f0fdc79d773226b769f9f93877e33fa9f8590
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-14 15:21:42 -0300

    importando a página SignIn no App

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import {
 import { NativeBaseProvider } from "native-base";
 import { Loading } from "./src/components/Loading";
 import { THEME } from "./src/theme";
+import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Hello World</Text> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
