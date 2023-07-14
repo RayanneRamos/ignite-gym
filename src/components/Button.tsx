f4df67a5e1e9647f25fc693c8eab10d23f682f2c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-14 15:52:41 -0300

    estilizando o componente Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,8 +6,19 @@ type Props = IButtonProps & {
 
 export function Button({ title, ...rest }: Props) {
   return (
-    <NativeBaseButton {...rest}>
-      <Text>{title}</Text>
+    <NativeBaseButton
+      w="full"
+      h={14}
+      bg="green.700"
+      rounded="sm"
+      _pressed={{
+        bg: "green.500",
+      }}
+      {...rest}
+    >
+      <Text color="white" fontFamily="heading" fontSize="sm">
+        {title}
+      </Text>
     </NativeBaseButton>
   );
 }
