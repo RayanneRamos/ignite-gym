8c426327dc330e3c72e61850ab3ea47046b65d70
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-17 16:42:19 -0300

    adicionando mais groups na página Home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { VStack } from "native-base";
+import { HStack, VStack } from "native-base";
 import { HomeHeader } from "../components/HomeHeader";
 import { Group } from "../components/Group";
 
@@ -6,7 +6,10 @@ export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Group name="costas" />
+      <HStack>
+        <Group name="costas" />
+        <Group name="ombro" />
+      </HStack>
     </VStack>
   );
 }
