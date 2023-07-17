c9630f677ceb7f48d8cf09f45298c93326215d6b
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-17 16:37:13 -0300

    utilizando o componente Group na página Home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,12 @@
-import { Center, Text, VStack } from "native-base";
+import { VStack } from "native-base";
 import { HomeHeader } from "../components/HomeHeader";
+import { Group } from "../components/Group";
 
 export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <Group name="costas" />
     </VStack>
   );
 }
