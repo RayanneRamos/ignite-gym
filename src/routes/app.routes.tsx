e4adcc1c1769a011850f6ca7bf5755ce4a36205c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-17 16:09:32 -0300

    estilizando o botão tabs

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -10,6 +10,7 @@ import HomeIconSvg from "../assets/home.svg";
 import HistoryIconSvg from "../assets/history.svg";
 import ProfileIconSvg from "../assets/profile.svg";
 import { useTheme } from "native-base";
+import { Platform } from "react-native";
 
 type AppRoutes = {
   home: undefined;
@@ -33,6 +34,13 @@ export function AppRoutes() {
         tabBarShowLabel: false,
         tabBarActiveTintColor: colors.green[500],
         tabBarInactiveTintColor: colors.gray[200],
+        tabBarStyle: {
+          backgroundColor: colors.gray[600],
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? "auto" : 96,
+          paddingBottom: sizes[10],
+          paddingTop: sizes[6],
+        },
       }}
     >
       <Screen
@@ -62,7 +70,11 @@ export function AppRoutes() {
           ),
         }}
       />
-      <Screen name="exercise" component={Exercise} />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{ tabBarButton: () => null }}
+      />
     </Navigator>
   );
 }
