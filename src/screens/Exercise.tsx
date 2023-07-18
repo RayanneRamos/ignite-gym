6c548448f6f279665b5d97b0e5bb966ee9068520
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-18 16:10:47 -0300

    imagem do exercício

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,4 +1,12 @@
-import { Center, HStack, Heading, Icon, Text, VStack } from "native-base";
+import {
+  Center,
+  HStack,
+  Heading,
+  Icon,
+  Text,
+  VStack,
+  Image,
+} from "native-base";
 import { TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
@@ -34,6 +42,19 @@ export function Exercise() {
             </Text>
           </HStack>
         </HStack>
+      </VStack>
+      <VStack p={8}>
+        <Image
+          w="full"
+          h={80}
+          source={{
+            uri: "https://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg",
+          }}
+          alt="Nome do exercício"
+          mb={3}
+          resizeMode="cover"
+          rounded="lg"
+        />
       </VStack>
     </VStack>
   );
