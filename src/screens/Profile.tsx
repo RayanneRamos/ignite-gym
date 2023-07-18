711f6c6a80aec709da989483951c878871492d74
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-18 15:27:37 -0300

    criando o botão alterar foto

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,6 +2,7 @@ import { Center, ScrollView, Text, VStack, Skeleton } from "native-base";
 import { ScreenHeader } from "../components/ScreenHeader";
 import { UserPhoto } from "../components/UserPhoto";
 import { useState } from "react";
+import { TouchableOpacity } from "react-native";
 
 const PHOTO_SIZE = 33;
 
@@ -28,6 +29,17 @@ export function Profile() {
               size={PHOTO_SIZE}
             />
           )}
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontSize="md"
+              fontWeight="bold"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
         </Center>
       </ScrollView>
     </VStack>
