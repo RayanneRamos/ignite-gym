afa231ba81ae6dbd7752640b1bc9152b39d05b20
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-19 16:13:58 -0300

    reajustes na interface de signUp

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -129,7 +129,7 @@ export function SignUp() {
           />
         </Center>
         <Button
-          mt={24}
+          mt={12}
           title="Voltar para o login"
           variant="outline"
           onPress={handleGoBack}
