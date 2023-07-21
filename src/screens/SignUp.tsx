5d0de03030ad3bff503b3fe9c90908070b0feb12
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-21 14:53:16 -0300

    criando o cadastro de um novo usuário

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -42,8 +42,17 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp(data: FormDataProps) {
-    console.log(data);
+  function handleSignUp({ name, email, password }: FormDataProps) {
+    fetch("http://192.168.0.26:3333/users", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   return (
