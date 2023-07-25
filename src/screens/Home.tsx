ed8df8329529e725475314251a30b78072d1582d
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-25 15:55:06 -0300

    criando o ExerciseDTO

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,11 +7,12 @@ import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "../routes/app.routes";
 import { api } from "../services/api";
 import { AppError } from "../utils/AppError";
+import { ExerciseDTO } from "../dtos/ExerciseDTO";
 
 export function Home() {
   const [groupSelected, setGroupSelected] = useState("costas");
   const [groups, setGroups] = useState<string[]>([]);
-  const [exercises, setExercises] = useState([]);
+  const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
   const navigation = useNavigation<AppNavigatorRoutesProps>();
   const toast = useToast();
 
@@ -94,7 +95,7 @@ export function Home() {
         </HStack>
         <FlatList
           data={exercises}
-          keyExtractor={(item) => item}
+          keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
             <ExerciseCard onPress={handleOpenExerciseDetails} />
           )}
