f5be8b2a3ca1268c3a0838f16708a91c465eb4e2
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-27 15:02:53 -0300

    listando o histórico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -12,18 +12,10 @@ import { useCallback, useState } from "react";
 import { AppError } from "../utils/AppError";
 import { api } from "../services/api";
 import { useFocusEffect } from "@react-navigation/native";
+import { HistoryByDayDTO } from "../dtos/HistoryByDayDTO";
 
 export function History() {
-  const [exercises, setExercises] = useState([
-    {
-      title: "26.08.22",
-      data: ["Puxada frontal", "Remada unilateral"],
-    },
-    {
-      title: "27.08.22",
-      data: ["Puxada frontal"],
-    },
-  ]);
+  const [exercises, setExercises] = useState<HistoryByDayDTO[]>([]);
   const toast = useToast();
   const [isLoading, setIsLoading] = useState(true);
 
@@ -32,7 +24,7 @@ export function History() {
       setIsLoading(true);
 
       const response = await api.get("/history");
-      console.log(response.data);
+      setExercises(response.data);
     } catch (error) {
       const isAppError = error instanceof AppError;
       const title = isAppError
@@ -59,7 +51,7 @@ export function History() {
       <ScreenHeader title="Histórico de Exercícios" />
       <SectionList
         sections={exercises}
-        keyExtractor={(item) => item}
+        keyExtractor={(item) => item.id}
         renderItem={({ item }) => <HistoryCard />}
         renderSectionHeader={({ section }) => (
           <Heading
