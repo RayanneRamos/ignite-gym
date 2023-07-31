138e12a904564f3a961b8df86f73e2d032f50f9d
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-31 15:48:20 -0300

    reenviando a requisição

--- src/services/api.ts
+++ src/services/api.ts
@@ -68,6 +68,23 @@ api.registerInterceptTokenManager = (signOut) => {
                 token: data.token,
                 refresh_token: data.refresh_token,
               });
+
+              if (originalRequestConfig.data) {
+                originalRequestConfig.data = JSON.parse(
+                  originalRequestConfig.data
+                );
+              }
+
+              originalRequestConfig.headers = {
+                Authorization: `Bearer ${data.token}`,
+              };
+              api.defaults.headers.common[
+                "Authorization"
+              ] = `Bearer ${data.token}`;
+
+              failedQueue.forEach((request) => {
+                request.onSuccess(data.token);
+              });
             } catch (error: any) {
               failedQueue.forEach((request) => {
                 request.onFailure(error);
